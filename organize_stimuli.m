25b2b3283b95ce78d9e0a95ce54161bacb52c183
Eric Tytell <eric.tytell@tufts.edu>
2014-06-09 11:13:22 -0400

    Organize and plot stimuli from pulse protocol

---
+++ organize_stimuli.m
@@ -0,0 +1,86 @@
+function data = organize_stimuli(data, varargin)
+
+opt.prestimcycles = 5;
+opt = parsevarargin(opt, varargin, 2);
+
+nchan = size(data.sig,2);
+stimper = 1/data.SinFreqStartHz;
+
+spikestimind = NaN(size(data.spiket));
+burststimind = NaN(size(data.burstt));
+for i = 1:nchan
+    good = isfinite(data.spiket(:,i));
+    spikestimind(good,i) = interp1(data.t,data.stimcycle, data.spiket(good,i));
+    
+    good = isfinite(data.burstt(:,i));
+    burststimind(good,i) = interp1(data.t,data.stimcycle, data.burstt(good,i));
+end
+
+burston = catuneven(1,data.burst.on)';
+burstoff = catuneven(1,data.burst.off)';
+
+stimdur = mode(round(diff(data.Time)/stimper));
+
+nstim = max(data.stimcycle);
+data.tstim = [];
+data.sigstim = [];
+data.angstim = [];
+data.spiketstim = [];
+data.bursttstim = [];
+data.burstcyclestim = [];
+data.burstonstim = [];
+data.burstdurstim = [];
+data.burstphasestim = [];
+for i = 1:nstim
+    tstart = data.Time(i) + data.BeforeDurSec;
+    tend = tstart + stimdur;
+    
+    %check that we're close to an even number of cycles, then round
+    %to an even number of cycles
+    assert(abs(round(tstart/stimper)*stimper - tstart) < 0.1*stimper);
+    tstart = round(tstart/stimper) * stimper;
+
+    t0 = tstart + 1/data.SinFreqStartHz;    % 1 period later
+    tstart = tstart - opt.prestimcycles/data.SinFreqStartHz;
+    
+    isstim = (data.t > tstart) & (data.t <= tend);
+    
+    sig1 = data.sig(isstim,:);
+    ang1 = data.ang(isstim);
+    t1 = data.t(isstim) - t0;
+    
+    spiket1 = [];
+    burstt1 = [];
+    burston1 = [];
+    burstdur1 = [];
+    burstphase1 = [];
+    for j = 1:nchan
+        isstim1 = (data.spiket(:,j) >= tstart) & (data.spiket(:,j) <= tend);
+        spiket1 = catuneven(2,spiket1,data.spiket(isstim1,j));
+        
+        isstim1 = (data.burstt(:,j) >= tstart) & (data.burstt(:,j) <= tend);
+        burstt1 = catuneven(2,burstt1,data.burstt(isstim1,j));
+        burston1 = catuneven(2,burston1,burston(isstim1,j));
+        burstdur1 = catuneven(2,burstdur1,burstoff(isstim1,j) - burston(isstim1,j));
+        burstphase1 = catuneven(2,burstphase1,data.burstphase(isstim1,j));
+    end
+    
+    spiket1 = spiket1 - t0;
+    burstt1 = burstt1 - t0;
+    burston1 = burston1 - t0;
+    
+    data.tstim = t1;
+    data.sigstim = catuneven(3,data.sigstim,sig1);
+    data.angstim = catuneven(3,data.angstim,ang1);
+    data.spiketstim = catuneven(3,data.spiketstim,spiket1);
+    data.bursttstim = catuneven(3,data.bursttstim,burstt1);
+    data.burstcyclestim = catuneven(3,data.burstcyclestim, floor(burstt1));
+    data.burstonstim = catuneven(3,data.burstonstim,burston1);
+    data.burstdurstim = catuneven(3,data.burstdurstim,burstdur1);
+    data.burstphasestim = catuneven(3,data.burstphasestim,burstphase1);
+end
+
+
+    
+    
+    
---
+++ plot_stimuli.m
@@ -0,0 +1,116 @@
+function plot_stimuli(data, varargin)
+
+assert(ismember(data.StimulusType, {'Pulses'}));
+
+opt.phase = [];
+opt.direction = [];
+opt.channel = [];
+opt.raster = true;
+opt.raw = true;
+opt.bursts = true;
+opt.meanburstphase = true;
+
+if (isnumeric(varargin{1}))
+    ind = varargin{1};
+    p = 2;
+else
+    ind = [];
+    p = 1;
+end
+
+opt = parsevarargin(opt,varargin(p:end), 2);
+
+if (isempty(ind))
+    if isempty(opt.phase)
+        opt.phase = [0 1];
+    elseif numel(opt.phase == 1)
+        opt.phase = [opt.phase opt.phase];
+    end
+
+    if isempty(opt.direction)
+        opt.direction = [-1 1];
+    elseif numel(opt.direction == 1)
+        opt.direction = [opt.direction opt.direction];
+    end
+    
+    ind = find((data.Phase >= opt.phase(1)) & (data.Phase <= opt.phase(2)) & ...
+        (data.Direction >= opt.direction(1)) & (data.Direction <= opt.direction(2)));
+end    
+
+if isempty(opt.channel)
+    opt.channel = 1:size(data.sig,2);
+end
+
+nchan = length(opt.channel);
+nrep = length(ind);
+
+h = -1*ones(nchan,1);
+
+if opt.meanburstphase
+    %most common maximum cycle number
+    ncycle = mode(flatten(max(data.burstcyclestim)));
+    
+    mnphase = zeros(1,nchan);
+    for c = 1:nchan
+        burstcycle1 = squeeze(data.burstcyclestim(:,c,:));
+        burstphase1 = squeeze(data.burstphasestim(:,c,:));
+        goodcyc = (burstcycle1 < -1) | ...
+            ((burstcycle1 > 4) & (burstcycle1 <= ncycle-1));
+        mnphase1 = angmean(2*pi*burstphase1(goodcyc));
+        mnphase(c) = mod(mnphase1/(2*pi),1);
+    end
+end
+
+clf;
+for i = 1:nchan
+    c = opt.channel(i);
+    
+    h(i) = subplot(nchan+1,1, i);
+    sig1 = squeeze(data.sigstim(:,c,ind));
+    if opt.raw
+        good = any(isfinite(sig1),2);
+        plot(data.tstim(good), sig1(good,:));
+    end
+    
+    d = max(abs(sig1(:)));
+    spiket1 = squeeze(data.spiketstim(:,c,ind));
+    spikey1 = d + (0:nrep-1)*d/5;
+
+    if opt.raster
+        addraster(spiket1, spikey1);
+    end
+
+    if opt.bursts
+        burst1 = cat(2,data.burstonstim(:,c,ind),...
+            data.burstonstim(:,c,ind)+data.burstdurstim(:,c,ind),...
+            NaN(size(data.bursttstim(:,c,ind))));
+        burst1 = permute(burst1,[2 1 3]);
+        burst1 = flatten(burst1,1:2);
+        
+        bursty1 = repmat(spikey1,[size(burst1,1) 1]);
+        
+        addplot(burst1,bursty1,'k-', 'LineWidth',1);
+        
+        burstctr1 = squeeze(data.bursttstim(:,c,ind));
+        bursty1 = repmat(spikey1,[size(burstctr1,1) 1]);
+        addplot(burstctr1,bursty1, 'ko', 'MarkerFaceColor','w');
+        if opt.meanburstphase
+            axis tight;
+            xl = xlim;
+            stimper = data.SinFreqStartHz;
+            xl = round(xl/stimper)*stimper;
+            
+            mnphase1 = (xl(1)+mnphase(i)*stimper):stimper:xl(2);
+            vertplot(mnphase1,'k-');
+        end
+    end
+    
+    axis tight;
+end
+
+h(i+1) = subplot(nchan+1,1, nchan+1);
+ang1 = squeeze(data.angstim(:,1,ind));
+good = any(isfinite(ang1),2);
+plot(data.tstim(good), ang1(good,:));
+
+linkaxes(h,'x');
