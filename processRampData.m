a08371f4189f295edcf6befc1f1a6f9efe28ba6d
Allan Yau <allan.yau@tufts.edu>
2014-10-23 10:35:47 -0400

    Additions to processRampData
    Added variables to analyze background signal

--- processRampData.m
+++ processRampData.m
@@ -12,6 +12,14 @@ spikerate = zeros(length(data.Time)*3, size(data.spiket,2));
 spikenum = zeros(length(data.Time)*3, size(data.spiket, 2));
 rampvel = zeros(length(data.Time)*3, 1);
 ramppos = zeros(length(data.Time)*3, 1);
+holdtime = zeros(length(data.Time)*3, 1);
+
+zerospikerate = zeros(length(data.Time), size(data.spiket, 2));
+zerospikenum = zeros(length(data.Time), size(data.spiket, 2));
+zerorampvel = zeros(length(data.Time), 1);
+zeroramppos = zeros(length(data.Time), 1);
+zeroholdtime = zeros(length(data.Time), 1);
+
 
 for i = 1:length(data.Time) %Steps through each start stimulus start t
     k = (i-1)*3; % Divides into the 3 phases
@@ -35,6 +43,7 @@ for i = 1:length(data.Time) %Steps through each start stimulus start t
     
     rampvel(k+1) = data.RampVel(i)*data.Direction(i);
     ramppos(k+1) = data.Amplitude(i)*data.Direction(i);
+    holdtime(k+1) = data.Duration(i);
     
     %Phase 2 of ramp
     t1 = t2;
@@ -49,6 +58,7 @@ for i = 1:length(data.Time) %Steps through each start stimulus start t
     
     rampvel(k+2) = 0;
     ramppos(k+2) = data.Amplitude(i)*data.Direction(i);
+    holdtime(k+2) = data.Duration(i);
     
     %Phase 3 of ramp
     t1 = t2;
@@ -63,8 +73,43 @@ for i = 1:length(data.Time) %Steps through each start stimulus start t
     
     rampvel(k+3) = -data.RampVel(i)*data.Direction(i);
     ramppos(k+3) = data.Amplitude(i)*data.Direction(i);
+    holdtime(k+3) = data.Duration(i);
 
+    %Zero spike rate (spike rate at neutral position)
+    if i == length(data.Time)
+        t1 = t2;
+        t2 = data.t(length(data.t));
+        
+         for j = 1:nchan
+             spikeind = find((data.spiket(:,j) >= t1) & ...
+                (data.spiket(:,j) <= t2));
+             zerospikenum(i,j) = length(spikeind);
+             zerospikerate(i,j) = length(spikeind)/(t2 - t1);
+         end
+
+         zerorampvel(i) = data.RampVel(i)*data.Direction(i);
+         zeroramppos (i) = data.Amplitude(i)*data.Direction(i);
+         zeroholdtime(i) = t2 - t1;
+         
+        break;
+    end
+    
+    t1 = t2;
+    t2 = data.Time(i+1) + data.BeforeDurSec + offset;
+
+    for j = 1:nchan
+        spikeind = find((data.spiket(:,j) >= t1) & ...
+            (data.spiket(:,j) <= t2));
+        zerospikenum(i,j) = length(spikeind);
+        zerospikerate(i,j) = length(spikeind)/(t2 - t1);
+    end
+
+    zerorampvel(i) = data.RampVel(i)*data.Direction(i);
+    zeroramppos (i) = data.Amplitude(i)*data.Direction(i);
+    zeroholdtime(i) = t2 - t1;
+    
 end
 
-putvar spikerate rampvel ramppos spikenum;
+putvar spikerate rampvel ramppos spikenum holdtime zerospikenum ...
+    zerospikerate zerorampvel zeroramppos zeroholdtime;
 
